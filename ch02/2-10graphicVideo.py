5b046f99c668bc525b94914f1b13235b2a77671d
heeone1 <rinakim1019@gmail.com>
2024-09-23 17:43:49 +0900

    마우스 이벤트

--- ch02/2-10graphicVideo.py
+++ ch02/2-10graphicVideo.py
@@ -12,15 +12,21 @@ while True:
         print('프레임 획득에 실패하여 루프를 나갑니다.')
         break
 
-#    pts = np.array([[180, 100], [190, 210], [300, 340]], dtype=np.int32)
-#    cv2.polylines(frame, [pts], False, (255, 0, 0), 10)
-#    cv2.line(frame, (400, 100), (640, 200), (0, 255, 0), 10)
+    # 고정되어있는것 처럼 보이는 선 그리기
+    pts = np.array([[180, 100], [190, 210], [300, 340]], dtype=np.int32)
+    cv2.polylines(frame, [pts], False, (255, 0, 0), 10)
+    cv2.line(frame, (400, 100), (640, 200), (0, 255, 0), 10)
+
+    #랜덤하게 원 그리기
+    y = np.random.rand() * frame.shape[0] #rand() : 0~1
+    x = np.random.rand() * frame.shape[1]
+    cv2.circle(frame, (int(x),int(y)), 20, (0,255,255), -1)
 
     cv2.imshow('Video display', frame)
     
     key=cv2.waitKey(1)	# 1밀리초 동안 키보드 입력 기다림
     if key==ord('q'):	# 'q' 키가 들어오면 루프를 빠져나감
-        break 
+        break
     
 cap.release()			# 카메라와 연결을 끊음
 cv2.destroyAllWindows()
